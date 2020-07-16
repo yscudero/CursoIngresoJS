e0deaf68d3079535427560a9660087301dbf8439
yscudero <yscudero@gmail.com>
2020-07-16 10:17:41 -0300

    Clase1

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,8 @@
+/*Comentarios*/
 /*
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Hola Mundo");
+	alert("Esto funciona de Maravilla");
 }
-
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,10 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
-}
+	var vNombre;
+	vNombre = prompt("Nombre","Ingrese un Nombre"); // asignando prompt a vNombre
+	alert(vNombre);
 
+    
+
+}
