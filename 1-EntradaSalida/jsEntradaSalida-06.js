36cbda86799903a04edc2cdd95c6a3fc57228c05
yscudero <yscudero@gmail.com>
2020-07-17 23:55:48 -0300

    Clase 3

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,15 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
+	var numUno;
+	var numDos;
+	var resul;
+
+	numUno = parseInt(document.getElementById("txtIdNumeroUno").value);
+    numDos = parseInt(document.getElementById("txtIdNumeroDos").value);
+	resul = numUno + numDos;
+
+	alert("La suma da " + resul);
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -4,22 +4,61 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
-	alert("ok");	
+{	let vNum1=40;
+	let vNum2=20;
+	let vResul;
+
+	vNum1 = document.getElementById("txtIdNumeroUno").value;
+	vNum1 = parseInt(vNum1);
+	vNum2 = parseInt( document.getElementById("txtIdNumeroDos").value );
+	vResul = vNum1 + vNum2;
+
+	alert("La suma es: "+vResul);
+
 }
 
 function restar()
-{
-	
+{  let vNumero1;
+	let vNumero2;
+	let vResul;
+
+	vNumero1 = document.getElementById("txtIdNumeroUno").value;
+	vNumero1 = parseInt(vNumero1);
+	vNumero2 = parseInt( document.getElementById("txtIdNumeroDos").value );
+
+	vResul = vNumero1 - vNumero2;
+
+	alert("La resta es: "+vResul);
+
 }
 
 function multiplicar()
 { 
-	
+	let vNumero1;
+	let vNumero2;
+	let vResul;
+
+	vNumero1 = document.getElementById("txtIdNumeroUno").value;
+	vNumero1 = parseInt(vNumero1);
+	vNumero2 = parseInt( document.getElementById("txtIdNumeroDos").value );
+
+	vResul = vNumero1 * vNumero2;
+
+	alert("La multiplicacion es: "+vResul);	
 }
 
 function dividir()
 {
-	
+	let vNumero1;
+	let vNumero2;
+	let vResul;
+
+	vNumero1 = document.getElementById("txtIdNumeroUno").value;
+	vNumero1 = parseFloat(vNumero1);
+	vNumero2 = parseFloat( document.getElementById("txtIdNumeroDos").value );
+
+	vResul = vNumero1 / vNumero2;
+
+	alert("La division es: "+ vResul.toFixed(2));	
 }
 
